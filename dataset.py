e0680408c77c7d8a651f8885636b362392824caa
lw@ylbcmi <longwei@ylbcmi.com>
2019-03-01 13:50:03 +0800

    add SIDataset & PJDataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import os
 import cv2
 import numpy as np
@@ -63,14 +62,138 @@ def filter_top_cv(d, k=10):
     return fd, top_cv
 
 
+def data_stat(stage=1):
+    d = load_by_stage(stage=stage)
+    # d, top_cv = filter_top_cv(d)
+    genes = list(d.keys())
+    imgs = []
+    for g in genes:
+        imgs.extend(d[g]['img'])
+    print("stage", stage, "genes", len(genes), "imgs", len(imgs))
+
+
+def generate_pj_samples(d, genes, count=4):
+    gene_imgs = []
+    for gene in genes:
+        ag_imgs = d[gene]['img']
+        chunk_imgs = [ag_imgs[i:i+count]
+                      for i in range(0, len(ag_imgs), count)]
+        gene_imgs.extend([(gene, c) for c in chunk_imgs])
+    return gene_imgs
+
+
+class SIDataset(Dataset):
+    '''Single Instance Dataset'''
+
+    def __init__(self, mode='train', stage=2):
+        super(SIDataset, self).__init__()
+        self.db, self.top_cv = filter_top_cv(load_by_stage(stage))
+        self.nclass = len(self.top_cv)
+        genes = list(self.db.keys())
+        tl = int(len(genes) * 0.5)
+        vl = int(len(genes) * 0.75)
+        if mode == 'train':
+            self.genes = genes[:tl]
+        elif mode == 'test':
+            self.genes = genes[vl:]
+        else:
+            self.genes = genes[tl:vl]
+
+        self.gene_imgs = [(gene, img) for gene in self.genes
+                          for img in self.db[gene]['img']]
+
+    def __len__(self):
+        return len(self.gene_imgs)
+
+    def __getitem__(self, idx):
+        gene, img = self.gene_imgs[idx]
+        imgpth = os.path.join('data/pic', img)
+        nimg = cv2.imread(imgpth, -1)
+        nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2RGB)
+        nimg = nimg.transpose(2, 0, 1)
+
+        gene_anns = self.db[gene]['ann']
+        anns = np.zeros(self.nclass)
+        for ann in gene_anns:
+            anns[self.top_cv.index(ann)] = 1
+
+        return gene, nimg, anns
+
+
+class PJDataset(Dataset):
+    '''Pin Jie Dataset as ltg does'''
+
+    def __init__(self, mode='train', stage=2):
+        super(PJDataset, self).__init__()
+        self.db, self.top_cv = filter_top_cv(load_by_stage(stage))
+        self.nclass = len(self.top_cv)
+        genes = list(self.db.keys())
+        tl = int(len(genes) * 0.5)
+        vl = int(len(genes) * 0.75)
+        if mode == 'train':
+            self.genes = genes[:tl]
+        elif mode == 'test':
+            self.genes = genes[vl:]
+        else:
+            self.genes = genes[tl:vl]
+
+        self.gene_imgs = generate_pj_samples(self.db, self.genes)
+
+    def __len__(self):
+        return len(self.gene_imgs)
+
+    def __getitem__(self, idx):
+        gene, imgs = self.gene_imgs[idx]
+
+        raw_nimgs = []
+        for img in imgs:
+            imgpth = os.path.join('data/pic', img)
+            nimg = cv2.imread(imgpth, -1)
+            nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2RGB)
+            raw_nimgs.append(nimg.transpose(2, 0, 1))
+        raw_nimgs = np.concatenate(raw_nimgs, axis=0)
+
+        n, c, h, w = raw_nimgs.shape
+        pj_nimg = np.zeros(c, 2*h, 2*w)
+        if n == 4:
+            pj_nimg[:, 0:h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, 0:h, w:2*w] = raw_nimgs[1]
+            pj_nimg[:, h:2*h, 0:w] = raw_nimgs[2]
+            pj_nimg[:, h:2*h, w:2*w] = raw_nimgs[3]
+        elif n == 3:
+            pj_nimg[:, 0:h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, 0:h, w:2*w] = raw_nimgs[1]
+            pj_nimg[:, h:2*h, 0:w] = raw_nimgs[2]
+            pj_nimg[:, h:2*h, w:2*w] = raw_nimgs[0]
+        elif n == 2:
+            pj_nimg[:, 0:h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, 0:h, w:2*w] = raw_nimgs[1]
+            pj_nimg[:, h:2*h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, h:2*h, w:2*w] = raw_nimgs[1]
+        elif n == 1:
+            pj_nimg[:, 0:h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, 0:h, w:2*w] = raw_nimgs[0]
+            pj_nimg[:, h:2*h, 0:w] = raw_nimgs[0]
+            pj_nimg[:, h:2*h, w:2*w] = raw_nimgs[0]
+        else:
+            raise Exception("invalid img numbers in PJDataset:", n)
+
+        gene_anns = self.db[gene]['ann']
+        anns = np.zeros(self.nclass)
+        for ann in gene_anns:
+            anns[self.top_cv.index(ann)] = 1
+
+        return gene, pj_nimg, anns
+
+
 class DrosophilaDataset(Dataset):
 
     def __init__(self, mode='train', stage=2):
         self.db, self.top_cv = filter_top_cv(load_by_stage(stage))
         self.nclass = len(self.top_cv)
         genes = list(self.db.keys())
-        tl = int(len(genes) * 0.6)
-        vl = int(len(genes) * 0.8)
+        tl = int(len(genes) * 0.5)
+        vl = int(len(genes) * 0.75)
         if mode == 'train':
             self.genes = genes[:tl]
         elif mode == 'test':
@@ -123,4 +246,5 @@ def fly_collate_fn(batch):
 
 
 if __name__ == "__main__":
-    pass
+    for s in list(range(1, 7)):
+        data_stat(s)
